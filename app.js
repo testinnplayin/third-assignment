136dbf1d344d3390488fd84c1459dcf04e49bc51
R Wood <archaurore@gmail.com>
2019-05-05 10:18:59 +0200

    created basic app

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ', PORT);
+});
